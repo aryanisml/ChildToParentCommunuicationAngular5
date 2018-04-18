9fa0713a04fab6dd88cab12a6b8e9218c80b4b91
Aryan <swapnil.kamble02@gmail.com>
2018-04-18 10:25:12 +0530

    Update sample.service.ts
    Variables setting in services

--- src/app/sample.service.ts
+++ src/app/sample.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SampleService {
    mySubject:Subject<any>=new Subject<any>();
 
-    constructor() { }
+   myInfo:any;
+   private _myInfo:Subject<any>=new Subject<any>();
+   _myInfo$:Observable<any>;
+
+    constructor() { 
+
+        this._myInfo$=this._myInfo.asObservable();
+        this._myInfo$.subscribe((data)=>{
+            this.myInfo=data;
+        })
+    }
 
     setData(data:any){
         this.mySubject.next(data);
+        this._myInfo.next(data);
     }
 
 }
